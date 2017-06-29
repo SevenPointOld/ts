d9293bb8ed32fa683ec61e30fd97593e34ca30c6
liujunchi <liujunchi@ifunq.com>
2017-06-29 10:11:02 +0800

    zkClient创建节点、读取节点子节点列表并注册Watcher

--- src/main/java/com/cqfq/ts/zk/SimpleZkClient.java
+++ src/main/java/com/cqfq/ts/zk/SimpleZkClient.java
@@ -1,9 +1,8 @@
 package com.cqfq.ts.zk;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +13,8 @@ import java.util.concurrent.TimeUnit;
  * @create 2017-06-28 11:45
  **/
 public class SimpleZkClient implements Watcher {
+
+    public static ZooKeeper zkClient = null;
 
     private  CountDownLatch latch;
 
@@ -27,7 +28,7 @@ public class SimpleZkClient implements Watcher {
          * 初始化客户端，这是只是发起连接请求就返回了，具体完成连接也就是建立会话是通过异步完成的
          * 这个通知会通过watcher回调到客户端
          */
-        ZooKeeper zkClient = new ZooKeeper("localhost:2181,localhost:2182,localhost:2183", 5000,
+        zkClient = new ZooKeeper("localhost:2181,localhost:2182,localhost:2183", 5000,
                 //这里注册一个watcher实例
                 new SimpleZkClient(latch));
         //获取客户端的连接状态
@@ -36,8 +37,67 @@ public class SimpleZkClient implements Watcher {
         //阻塞等待，直到会话建立成功
         latch.await();
 
-        System.out.println("zkClient session 创建成功");
+        //获取客户端的连接状态
         System.out.println("zk客户端状态：" + zkClient.getState());
+
+        //获取客户端Id和秘钥
+        long sesstionId = zkClient.getSessionId();
+        byte[] sesstionPasswd = zkClient.getSessionPasswd();
+        /**
+         * 客户端会话创建 end
+         */
+
+
+        /**
+         * 节点创建 begin
+         */
+        //同步创建数据节点
+        String path = zkClient.create("/myNodeSyn",
+                "hello zk!".getBytes("utf-8"),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+        System.out.println("同步创建持久节点 path:" + path);
+        //同步创建子节点，这里要注意，如果父节点是临时节点的话 是不允许创建子节点的
+        String childPath = zkClient.create("/myNodeSyn/child",
+                "hello zk!".getBytes("utf-8"),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL);
+        System.out.println("同步创建临时节点 path:" + childPath);
+
+
+        //异步创建一个数据节点。
+        zkClient.create("/myNodeAyn",
+                "hello zk!".getBytes("utf-8"),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                new SimpleZkClient.MyStringCallBack(),
+                "我是上下文~~~~");
+        /**
+         * 节点创建 end
+         */
+
+
+        /**
+         * 节点读取 begin
+         */
+        //同步获取节点的一级子节点列表，并启用默认Watcher(就是实例化ZK客户端时的Watcher)
+        List<String> childNodeList = zkClient.getChildren(path, true);
+        System.out.println("获取子节点 " + childNodeList);
+        //这里创建一个子节点，验证是否通知对应事件类型给客户端
+        zkClient.create(path + "/child2",
+                "hello zk!".getBytes("utf-8"),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL);
+
+        /**
+         * 节点读取 end
+         */
+
+
+
+
+
+
         TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
 
@@ -47,10 +107,43 @@ public class SimpleZkClient implements Watcher {
      * @param watchedEvent
      */
     public void process(WatchedEvent watchedEvent) {
-        //输入接收到事件通知
+        //输出接收到事件通知
         System.out.println("接收到事件：" + watchedEvent);
         if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
-            latch.countDown();
+            if (watchedEvent.getType() == Event.EventType.None && null == watchedEvent.getPath()) {
+                //会话创建完成通知
+                latch.countDown();
+            } else if (watchedEvent.getType() == Event.EventType.NodeChildrenChanged) {
+                //子节点列表变动通知
+                try {
+                    //重新同步获取子节点，并注册默认Watcher
+                    List<String> childNodeList = zkClient.getChildren(watchedEvent.getPath(), true);
+                    System.out.println("重新获取子节点 " + childNodeList);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 回调对象
+     */
+    public static class MyStringCallBack implements AsyncCallback.StringCallback {
+
+        /**
+         *
+         * @param rc    响应码 0：成功，-4：客户端和服务端连接已断开，-110：指定节点已存在，-112：会话已过期
+         * @param path  客户端调用API传入的path
+         * @param ctx   客户端调用API传入的上下文
+         * @param name  ZK服务端响应的实际path（这是因为创建顺序节点的path客户端在节点创建成功之前是不知道的，ZK服务端为顺序节点的path加上了后缀）
+         */
+        public void processResult(int rc, String path, Object ctx, String name) {
+            System.out.println("异步创建临时节点 rc=" + rc
+                    + " path=" + path
+                    + " ctx=" + ctx
+                    + " name=" + name);
         }
     }
 }
