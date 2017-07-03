eca35b5a2d0ce7235c886f44dd6034002367c433
liujunchi <liujunchi@ifunq.com>
2017-07-03 16:42:29 +0800

    zookeeper原生api 客户端demo完成

--- src/main/java/com/cqfq/ts/zk/SimpleZkClient.java
+++ src/main/java/com/cqfq/ts/zk/SimpleZkClient.java
@@ -1,6 +1,7 @@
 package com.cqfq.ts.zk;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -41,8 +42,8 @@ public class SimpleZkClient implements Watcher {
         System.out.println("zk客户端状态：" + zkClient.getState());
 
         //获取客户端Id和秘钥
-        long sesstionId = zkClient.getSessionId();
-        byte[] sesstionPasswd = zkClient.getSessionPasswd();
+//        long sesstionId = zkClient.getSessionId();
+//        byte[] sesstionPasswd = zkClient.getSessionPasswd();
         /**
          * 客户端会话创建 end
          */
@@ -55,7 +56,7 @@ public class SimpleZkClient implements Watcher {
         String path = zkClient.create("/myNodeSyn",
                 "hello zk!".getBytes("utf-8"),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                //只有临时节点，才允许创建子节点
+                //只有持久节点，才允许创建子节点
                 CreateMode.PERSISTENT);
         System.out.println("同步创建持久节点 path:" + path);
         //同步创建子节点，这里要注意，如果父节点是临时节点的话 是不允许创建子节点的
@@ -88,7 +89,7 @@ public class SimpleZkClient implements Watcher {
 
         //这里创建一个子节点，验证是否通知对应事件类型给客户端
         zkClient.create(path + "/child2",
-                "hello zk!".getBytes("utf-8"),
+                "hello zk i'm childrenNode!".getBytes("utf-8"),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL);
         /**
@@ -100,12 +101,35 @@ public class SimpleZkClient implements Watcher {
          * 读取节点数据 begin
          */
         //同步读取节点数据
-
+        Stat stat = new Stat();
+        byte[] bytes = zkClient.getData(childPath, true, stat);//启用默认Watcher 接收节点数据变更事件
+        System.out.println("同步读取节点数据 path:" + childPath + " data:" + new String(bytes));
+        System.out.println("节点状态 " + stat.getCzxid() + "," + stat.getMzxid() + "," + stat.getVersion());
 
         /**
          * 读取节点数据 end
          */
 
+
+        /**
+         * 更新节点数据 begin
+         */
+        stat = zkClient.setData(childPath, "changed again!".getBytes(), stat.getVersion());
+        System.out.println("同步更新后节点状态 " + stat.getCzxid() + "," + stat.getMzxid() + "," + stat.getVersion());
+
+        /**
+         * 更新节点数据 end
+         */
+
+
+        /**
+         * 检测节点是否存在 begin
+         */
+        zkClient.exists("/existsTestNode", true);
+
+        /**
+         * 检测节点是否存在 end
+         */
 
         TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
@@ -128,6 +152,35 @@ public class SimpleZkClient implements Watcher {
                     //重新同步获取子节点，并注册默认Watcher
                     List<String> childNodeList = zkClient.getChildren(watchedEvent.getPath(), true);
                     System.out.println("重新同步获取子节点 " + childNodeList);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (watchedEvent.getType() == Event.EventType.NodeDataChanged) {
+                //节点状态变更通知(这里节点的状态不单单指节点的数据，可能修改后数据与修改前数据一致，但是版本信息发生了改变，也会发送通知)
+                try {
+                    Stat stat = new Stat();
+                    byte[] bytes = zkClient.getData(watchedEvent.getPath(), false, stat);
+                    zkClient.exists(watchedEvent.getPath(), true);//重新注册watcher
+                    System.out.println("重新同步读取节点数据 path:" + watchedEvent.getPath() + " data:" + new String(bytes));
+                    System.out.println("节点变更后的状态 " + stat.getCzxid() + "," + stat.getMzxid() + "," + stat.getVersion());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (watchedEvent.getType() == Event.EventType.NodeCreated) {
+                //节点创建通知
+                try {
+                    byte[] bytes = zkClient.getData(watchedEvent.getPath(), false, new Stat());
+                    System.out.println("创建节点 path:" + watchedEvent.getPath() + " data:" + new String(bytes));
+                    zkClient.exists(watchedEvent.getPath(), true);//重新注册watcher
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else if (watchedEvent.getType() == Event.EventType.NodeDeleted) {
+                //节点删除通知
+                System.out.println("删除节点 path:" + watchedEvent.getPath());
+                try {
+                    zkClient.exists(watchedEvent.getPath(), true);//重新注册watcher
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
